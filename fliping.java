fd83018931e4ea0cbdc22fe874c474de33926e7a
manisha4450 <chowdaryc226@gmail.com>
2024-12-03 15:04:34 +0530

    Add files via upload

---
+++ fliping.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class fliping{
+    public static void main(String[] args){
+        //flipping bits
+        int a=12;
+        int b=30;
+        System.out.println(a^(1<<b)); 
+    }
+}
