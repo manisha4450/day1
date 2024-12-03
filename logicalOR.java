bbb1c3dfc8d1d474a802c153d88e6adf5163d328
manisha4450 <chowdaryc226@gmail.com>
2024-12-03 15:52:01 +0530

    Add files via upload

---
+++ logicalOR.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class logicalOR{
+    public static void main(String[] args){
+        System.out.println(true||true);
+        System.out.println(false||true);
+        System.out.println(true||false);
+        System.out.println(false||false);
+    }
+}
