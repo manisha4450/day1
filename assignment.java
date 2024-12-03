013431ac41ff35d4aa549fcf324f75a14c4ce69d
manisha4450 <chowdaryc226@gmail.com>
2024-12-03 12:02:38 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,9 @@
+class assignment {
+    public static void main(String[] args){
+      int a=20;
+      System.out.println(a+=30);
+      System.out.println(a-=9);
+      System.out.println(a*=2);  
+    }
+
+}
